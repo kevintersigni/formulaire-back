c0b360e59401c5e838b971862f17fcc18dd4e00f
Kevin Tersigni <kevintersigni@macbook-air-de-kevin.home>
2021-06-19 18:39:42 +0200

    replace var by constg

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const formidable = require("express-formidable");
 const cors = require("cors");
 
-var API_KEY = process.env.MAILGUN_API_KEY;
-var DOMAIN = process.env.MAILGUN_DOMAIN;
-var mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
+const API_KEY = process.env.MAILGUN_API_KEY;
+const DOMAIN = process.env.MAILGUN_DOMAIN;
+const mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
 
 const app = express();
 
